cf122842113b4730dc187bd01fdbd266c3a8a1fb
mariocamacho93 <92121727+mariocamacho93@users.noreply.github.com>
2021-11-10 12:47:41 -0500

    Create date_params.sql

---
+++ date_params.sql
@@ -0,0 +1,6 @@
+select event_name,
+       count(distinct user_pseudo_id) as users
+from `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+WHERE PARSE_DATE('%Y%m%d', _TABLE_SUFFIX) > PARSE_DATE('%Y%m%d', @DS_START_DATE)
+AND PARSE_DATE('%Y%m%d', _TABLE_SUFFIX) < PARSE_DATE('%Y%m%d', @DS_END_DATE)
+GROUP BY EVENT_NAME LIMIT 100; 
